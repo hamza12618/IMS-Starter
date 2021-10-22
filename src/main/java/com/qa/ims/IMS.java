bbe4f378afa2ed1e62a4c48bc55037486b290276
hamza12618 <hamza_12618@live.com>
2021-10-22 11:20:20 +0100

    IMS-25 #comment added ERD FINAL draft and initial

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -50,10 +50,13 @@ public class IMS {
 				active = this.customers;
 				break;
 			case ITEM:
+				active = this.items;
 				break;
 			case ORDER:
+				active = this.orders;
 				break;
 			case STOP:
+				active = this.stop;
 				return;
 			default:
 				break;
