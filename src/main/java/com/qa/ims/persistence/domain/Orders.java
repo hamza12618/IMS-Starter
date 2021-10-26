f85249ab238e348b7fb8fa5cffafef457b1b0137
hamza12618 <hamza_12618@live.com>
2021-10-26 12:40:00 +0100

    IMS-23 #comment added OrderDAO

--- src/main/java/com/qa/ims/persistence/domain/Orders.java
+++ src/main/java/com/qa/ims/persistence/domain/Orders.java
@@ -6,96 +6,71 @@ public class Orders {
 	private Customer customer;
 	private Double totalPrice;
 	private int quantity;
+	private Item item;
 	
-	
-	public Orders(Long orderId, Customer customer, Double totalPrice, int quantity) {
+	public Orders(Long orderId, Customer customer, Double totalPrice, int quantity, Item item) {
 		super();
 		this.orderId = orderId;
 		this.customer = customer;
 		this.totalPrice = totalPrice;
 		this.quantity = quantity;
+		this.item = item;
 	}
-
-
-
-	public Orders(Customer customer, Double totalPrice, int quantity) {
+	public Orders(Customer customer, Double totalPrice, int quantity, Item item) {
 		super();
 		this.customer = customer;
 		this.totalPrice = totalPrice;
 		this.quantity = quantity;
+		this.item = item;
 	}
-
 
 
 	public Long getOrderId() {
 		return orderId;
 	}
-
-
-
 	public void setOrderId(Long orderId) {
 		this.orderId = orderId;
 	}
-
-
-
 	public Customer getCustomer() {
 		return customer;
 	}
-
-
-
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-
-
-
 	public Double getTotalPrice() {
 		return totalPrice;
 	}
-
-
-
 	public void setTotalPrice(Double totalPrice) {
 		this.totalPrice = totalPrice;
 	}
-
-
-
 	public int getQuantity() {
 		return quantity;
 	}
-
-
-
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-
-
-
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
 	@Override
 	public String toString() {
 		return "Orders [orderId=" + orderId + ", customer=" + customer + ", totalPrice=" + totalPrice + ", quantity="
-				+ quantity + "]";
+				+ quantity + ", item=" + item + "]";
 	}
-
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((item == null) ? 0 : item.hashCode());
 		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
 		result = prime * result + quantity;
 		result = prime * result + ((totalPrice == null) ? 0 : totalPrice.hashCode());
 		return result;
 	}
-
-
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -109,6 +84,11 @@ public class Orders {
 			if (other.customer != null)
 				return false;
 		} else if (!customer.equals(other.customer))
+			return false;
+		if (item == null) {
+			if (other.item != null)
+				return false;
+		} else if (!item.equals(other.item))
 			return false;
 		if (orderId == null) {
 			if (other.orderId != null)
@@ -124,12 +104,8 @@ public class Orders {
 			return false;
 		return true;
 	}
-
-
-
-	
-	
-
 	
 	
 }
+	
+	
